1990dd5587786d9bb53a4487ee579c18199625f1
mlippautz <mlippautz@users.noreply.github.com>
2026-07-20 20:50:41 +0000

    deploy: 5696dd16c84742f0f055a745bb2886912b1278e1

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.DRIVE_APP_CONFIG = { API_KEY: 'AIzaSyA_PHWk1YRMncae4seWddP9JbRgrl98-9Y', CLIENT_ID: '268642023464-mqd1c8gqedchsh8fakj843u888s2fgpn.apps.googleusercontent.com' };
+window.DRIVE_APP_CONFIG = { CLIENT_ID: '268642023464-mqd1c8gqedchsh8fakj843u888s2fgpn.apps.googleusercontent.com' };
